2dc3a2d498e824b8e65d46c726bd6fc63f7f3fec
Root@Cartoon <78117846+rootcartoon@users.noreply.github.com>
2024-09-04 19:08:46 -0300

    Add files via upload

---
+++ ct-manutencao.lua
@@ -0,0 +1,14 @@
+--------------------------------------------------------------------------------
+------------------------------CÓDIGO DE MANUTENÇÃO------------------------------
+---------------------------------CARTOON NETWORK--------------------------------
+--------------------------------------------------------------------------------
+
+
+-- LOCAL DE INSTALAÇÃO: vrp/config/Global.lua
+
+Maintenance = false -- false = desligado / true = ligado
+MaintenanceText = "Servidor em manutenção" -- Texto que irá aparecer na tela do fivem, quando tentar conectar sem permissão
+MaintenanceLicenses = { 
+	["sua steam hex"] = false -- false = desligado / true = ligado
+    -- OBS: quando o script estiver ligado, só irá conectar quem tiver com a license cadastrada
+}
